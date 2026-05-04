ae76ecc1f6772aad81daeed7a9987a57c505ee25
vineet rathor <vineetrathor57@gmail.com>
2026-05-04 16:57:17 +0530

    Added base springboot project version-3.5 with java 21

---
+++ cache-project/src/main/java/com/vineet/cache_project/CacheProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vineet.cache_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheProjectApplication.class, args);
+	}
+
+}
---
+++ cache-project/src/test/java/com/vineet/cache_project/CacheProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineet.cache_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
